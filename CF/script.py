3aee6f937f18a58ccd4f5b516d7eff2e908db15b
alphaaviral <alphaaviral@gmail.com>
2021-02-28 20:06:52 +0530

    navigate to site

--- CF/script.py
+++ CF/script.py
@@ -6,4 +6,10 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 
+contestNo =str(input("Input the contest number: "))
+url = "https://codeforces.com/problemset/problem/" + contestNo +"/A"
 driver = webdriver.Chrome(PATH)
+
+
+
+driver.get(url)
